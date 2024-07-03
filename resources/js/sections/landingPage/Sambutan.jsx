101035a34be570b0514138789f3928bf426e3052
anantariskys <gamersindo555@gmail.com>
2024-07-03 16:59:08 +0700

    Update Sambutan.jsx

--- resources/js/sections/landingPage/Sambutan.jsx
+++ resources/js/sections/landingPage/Sambutan.jsx
@@ -7,7 +7,7 @@ const Sambutan = () => {
             <main className="w-full lg:border-x-2 border-[#006181] relative gap-32 lg:gap-10 lg:border-b-2 container mx-auto lg:px-16 md:px-10 px-4 flex lg:flex-row flex-col rounded-3xl  py-32 lg:p-10">
                 <div className=" lg:w-2/4 relative z-30 flex flex-col justify-center items-center">
                     <img src={orang} alt="" className="w-[70%]" draggable='false' />
-                    <div className="flex bg-[#4C2C17] w-full p-4 rounded-2xl shadow-md text-white">
+                    <div className="flex bg-[#4C2C17] font-inter w-full p-4 rounded-2xl shadow-md text-white">
                         <div className="flex flex-col w-1/2 items-center">
                             <h1 className="lg:text-xl text-base font-semibold">Bram Adi Baskoro</h1>
                             <p className="lg:text-base text-sm text-center">Wakil Presiden BEM FISIP UB 2024</p>
