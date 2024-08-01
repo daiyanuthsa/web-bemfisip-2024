91a5ab76cde1452b0d75046574937f549b25dae1
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2024-08-01 21:01:55 +0700

    feat: Add Hotjar

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,6 +5,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import Hotjar from "@hotjar/browser";
+
+const siteId = 5080217;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./pages/${name}.jsx`, import.meta.glob('./pages/*.jsx')),
